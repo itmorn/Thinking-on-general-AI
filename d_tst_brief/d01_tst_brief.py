cb6792b9eb1bdad8bc90f77d0a2890bdf4b022ae
itmorn <12567148@qq.com>
2022-09-05 10:27:47 +0800

    使用BRIEF描述符。 考虑如何检测模板特征点 以及 特征点之间的空间联系

--- d_tst_brief/d01_tst_brief.py
+++ d_tst_brief/d01_tst_brief.py
@@ -4,10 +4,10 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 # img = cv.imread('../a01_test_canny/123.jpg',0)
-img = cv.imread('9.png',0)
+img = cv.imread('../images/12.jpg',0)
 # 初始化FAST检测器
 fast = cv.FastFeatureDetector_create()
-brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
+brief = cv.xfeatures2d.BriefDescriptorExtractor_create(16)
 
 a = time.time()
 kp = fast.detect(img,None)
